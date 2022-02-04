ba7bfdd811f9c8b4018a9545f77433e936db2bf3
ranjanapul <49339006+ranjanapul@users.noreply.github.com>
2022-02-04 19:28:18 +0530

    Delete atmCard.py

--- atmCard.py
+++
@@ -1,7 +0,0 @@
-class AtmCard:
-
-    def __init__(self, accountNumber):
-        self.acccountNumber = accountNumber
-    #Ignoring attributes like CVV,bankname,username,etc. for the sake of simplicity
-
-    
